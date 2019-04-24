233f979f2cd7a8a3044ac3fb0816ec66a011f58e
zgzjj <252070739@qq.com>
2019-04-25 00:14:56 +0800

    实现首页icons滑动分页

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,52 +1,115 @@
 <template>
     <div class="icons">
-      <div class="icon">
-        <div class="icon-img">
-          <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"/>
-        </div>
-        <p class="icon-desc">景点门票</p>
-      </div>
+      <swiper>
+        <swiper-slide v-for="(page, index) of pages" :key="index">
+          <div class="icon" v-for="item of page" :key="item.id">
+            <div class="icon-img">
+              <img class="icon-img-content" :src="item.imgUrl"/>
+            </div>
+            <p class="icon-desc">{{item.desc}}</p>
+          </div>
+        </swiper-slide>
+      </swiper>
     </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeIcons'
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+        desc: '一日游'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+        desc: '成都必游'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/e3/67df61427c8e1302.png',
+        desc: '川剧变脸'
+      }, {
+        id: '0005',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/f04285731d7121da1b9028e2bf431695.png',
+        desc: '都江堰'
+      }, {
+        id: '0006',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/50/26ffa31b56646402.png',
+        desc: '亲子游'
+      }, {
+        id: '0007',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/8246f27355943202.png',
+        desc: '游乐场'
+      }, {
+        id: '0008',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/54/35899492b1302802.png',
+        desc: '熊猫基地'
+      }, {
+        id: '0009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png',
+        desc: '动植物园'
+      }, {
+        id: '0010',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/bd/9f7b9b2b60c1502.png',
+        desc: '成都赏花'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
   @import '~styles/varibles.styl'
-  .icons
-    overflow :hidden;
+  @import '~styles/mixins.styl'
+  .icons >>> .swiper-container
     height :0;
     padding-bottom :50%;
-    .icon
-      position :relative
-      overflow :hidden;
-      float :left;
-      width :25%;
-      height :0;
-      padding-bottom :25%;
-      .icon-img
-        position :absolute;
-        top :0;
-        left :0;
-        right :0;
-        bottom :.44rem;
-        box-sizing :border-box;
-        padding :.1rem;
-        .icon-img-content
-          display :block;
-          margin :0 auto;
-          height :100%;
-    .icon-desc
+  .icon
+    position :relative
+    overflow :hidden;
+    float :left;
+    width :25%;
+    height :0;
+    padding-bottom :25%;
+    .icon-img
       position :absolute;
+      top :0;
       left :0;
       right :0;
-      bottom :0;
-      height :.44rem;
-      line-height :.44rem;
-      text-align :center;
-      color :$darkTextColor;
+      bottom :.44rem;
+      box-sizing :border-box;
+      padding :.1rem;
+      .icon-img-content
+        display :block;
+        margin :0 auto;
+        height :100%;
+  .icon-desc
+    position :absolute;
+    left :0;
+    right :0;
+    bottom :0;
+    height :.44rem;
+    line-height :.44rem;
+    text-align :center;
+    color :$darkTextColor;
+    ellipse()
 </style>
