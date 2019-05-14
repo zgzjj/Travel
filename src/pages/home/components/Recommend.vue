219aeab68ef87cc7a219d1ee67c2f938c41178e0
zgzjj <252070739@qq.com>
2019-05-14 14:38:59 +0800

    热销推荐组件

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img class="item-img" :src="item.imgUrl"  />
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1612/1d/1d9a740c1f9e0efaa3.img.jpg_200x200_63285bd9.jpg',
+        title: '成都海昌极地海洋公园',
+        desc: '成都的标志，也是现代人对老成都的记忆'
+      }, {
+        id: '002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1612/1d/1d9a740c1f9e0efaa3.img.jpg_200x200_63285bd9.jpg',
+        title: '成都海昌极地海洋公园',
+        desc: '成都的标志，也是现代人对老成都的记忆'
+      }, {
+        id: '003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1612/1d/1d9a740c1f9e0efaa3.img.jpg_200x200_63285bd9.jpg',
+        title: '成都海昌极地海洋公园',
+        desc: '成都的标志，也是现代人对老成都的记忆'
+      }, {
+        id: '004',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1612/1d/1d9a740c1f9e0efaa3.img.jpg_200x200_63285bd9.jpg',
+        title: '成都海昌极地海洋公园',
+        desc: '成都的标志，也是现代人对老成都的记忆'
+      }, {
+        id: '005',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1612/1d/1d9a740c1f9e0efaa3.img.jpg_200x200_63285bd9.jpg',
+        title: '成都海昌极地海洋公园',
+        desc: '成都的标志，也是现代人对老成都的记忆'
+      }, {
+        id: '006',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1612/1d/1d9a740c1f9e0efaa3.img.jpg_200x200_63285bd9.jpg',
+        title: '成都海昌极地海洋公园',
+        desc: '成都的标志，也是现代人对老成都的记忆'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    overflow :hidden
+    display :flex
+    height :1.9rem
+    .item-img
+      width :1.7rem
+      height :1.7rem
+      padding :.1rem
+    .item-info
+      flex :1
+      padding :.1rem
+      min-width :0
+      .item-title
+        line-height :.54rem
+        font-size :.32rem
+        ellipse()
+      .item-desc
+        line-height :.4rem
+        color: #ccc
+        ellipse()
+      .item-button
+        line-height :.44rem
+        margin-top: .16rem
+        background :#ff9300
+        padding :0 .1rem
+        border-radius :.06rem
+        color :#ffffff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl"  />
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [{
+        id: '001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/a2/78e4dbcfd45a6d.jpg_r_640x214_7a62b06a.jpg',
+        title: '成都海昌极地海洋公园',
+        desc: '成都的标志，也是现代人对老成都的记忆'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item-img-wrapper
+    overflow :hidden
+    height :0
+    padding-bottom :33.9%
+  .item-img
+    width :100%
+  .item-info
+    padding :.1rem
+    .item-title
+      line-height :.54rem
+      font-size :.32rem
+      ellipse()
+    .item-desc
+      line-height :.4rem
+      color: #ccc
+      ellipse()
+</style>
